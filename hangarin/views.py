acf277482f98c28e721b9674c0e7237c2f91035d
Esumandi <202380248@PSU.PALAWAN.EDU.PH>
2025-10-14 09:33:49 +0800

    Pt3

--- hangarin/views.py
+++ hangarin/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+#from .models import
 
 # Create your views here.
+
+def home(request):
+    """Render the application homepage."""
+    return render(request, 'home.html')

5640cc3692944211ca754a66fc174d757e71093e
Esumandi <202380248@PSU.PALAWAN.EDU.PH>
2025-10-15 20:23:18 +0800

    pt6 google only

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 from hangarin.views import HomePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    path('', RedirectView.as_view(pattern_name='account_login', permanent=False)),
     path('', include('hangarin.urls')),
+    path("accounts/", include("allauth.urls")), # allauth routes
 ]
